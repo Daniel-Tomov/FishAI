477137de65dcd1aca04bf7eb235d44bebddc587d
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-05-20 09:27:17 -0400

    add 2nd video

--- yolo/tranferImgs.py
+++ yolo/tranferImgs.py
@@ -1,9 +1,13 @@
 from os import listdir
+import pathlib
 import os
 from os.path import isfile, join
 mypath = 'fish_data/labels/validation'
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 for i in onlyfiles:
-  os.system("copy " + "fish_data/images/train/" + str(i).replace("txt", ".png") + " " + "fish_data/images/validation")
+  if "two" in i:
+    trainPath = str(pathlib.PureWindowsPath(r'fish_data/images/train/' + i.replace("txt", "png")))
+    valPath = str(pathlib.PureWindowsPath(r'fish_data/images/validation/' + i.replace("txt", "png")))
+    os.system("copy " + trainPath + " " + valPath)
 
